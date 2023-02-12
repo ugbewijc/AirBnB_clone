6a61961d69ba3c584a779b81d1c22596ae1059fb
John Chioma Ugbewi <ugbewijc@gmail.com>
2023-02-12 14:06:35 -0800

    testing

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 """defines Amenity Class"""
-
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
     """Amenity Class"""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
